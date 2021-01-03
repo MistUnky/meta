984cd970010d475ed39ae831b1f008e3e490c900
MistUnky <misterunknown128@gmail.com>
2021-01-03 11:59:50 -0500

    added nop for cycling operations without alteration or I/O

--- prng.c
+++ prng.c
@@ -7,11 +7,12 @@ main()
 	srand(23);
 	while(1)
 	{
-		switch(getchar() % 4) {
+		switch(getchar() % 5) {
 			case 0: p = map[p]; break;
 			case 1: if(map[p] != MAP_SZ-1) map[p]++; break;
 			case 2: if(map[p] != 0) map[p]--; break;
 			case 3: putchar(map[p]%256); fflush(stdout); break;
+			case 4: 0; break;
 		}
 	}
 }
