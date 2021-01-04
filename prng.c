1ee39f99a4dd82e126d5437f579cf79280342791
MistUnky <misterunknown128@gmail.com>
2021-01-03 21:05:26 -0500

    Add Allow Status for NOPing and to prevent unnecessary IO repetition

--- prng.c
+++ prng.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h>
 #define MAP_SZ 2323//2323
 int p = 0;
 int map[MAP_SZ];
+bool io_allow_nop = true;
 main()
 {
 	srand(23);
-	while(1)
+	while(true)
 	{
-		switch(getchar() % 5) {
-			case 0: p = map[p]; break;
-			case 1: if(map[p] != MAP_SZ-1) map[p]++; break;
-			case 2: if(map[p] != 0) map[p]--; break;
-			case 3: putchar(map[p]%256); fflush(stdout); break;
-			case 4: 0; break;
+		switch(getchar() % 4) {
+			case 0: p = map[p]; io_allow_nop = true; break;
+			case 1: if(map[p] != MAP_SZ-1) map[p]++; io_allow_nop = true; break;
+			case 2: if(map[p] != 0) map[p]--; io_allow_nop = true; break;
+			case 3: if(io_allow_nop) { putchar(map[p]%256); fflush(stdout); io_allow_nop = false; } break;
 		}
 	}
 }
